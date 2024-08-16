7d606bdfead847896ebe91aadb048f9577aab3b2
Abner Sebick <abn.sebick@gmail.com>
2024-08-16 14:57:13 -0400

    fix url

--- api_controllers/transactions.py
+++ api_controllers/transactions.py
@@ -2,7 +2,8 @@ import requests
 import os
 
 def get_transactions_by_account_from_api(account_id: str, link_id: str):
-    url = f'{os.environ['BELVO_MOVEMENTS_LIST']}&link={link_id}&account={account_id}'
+    base_url = os.environ['BELVO_MOVEMENTS_LIST']
+    url = f'{base_url}&link={link_id}&account={account_id}'
     print('url to get movements')
     print(url)
     accounts_response = requests.get(
